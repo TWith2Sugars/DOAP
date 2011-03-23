f0ff2e020e8a330e4b8ecf14831e3e0ee641086a
Tony Williams <tonywilliams1986@gmail.com>
2011-03-23 14:55:13 +0000

    forgot to include files

---
+++ src/DOAP.WCFExample/OAuthService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Web;
+using DOAP.Context;
+using DOAP.Response;
+using DOAP.WCFExample.Provider;
+
+namespace DOAP.WCFExample
+{
+  [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+  [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+  [ServiceContract]
+  public class OAuthService 
+  {
+    private readonly OAuthProvider<int, int> oAuthProvider;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OAuthService"/> class.
+    /// </summary>
+    public OAuthService()
+    {
+      var clientProvider = new ClientProvider();
+      var tokenProvider = new TokenProvider();
+      var passwordProvider = new PasswordProvider();
+      
+      
+      var scopes = new List<string>();
+
+      this.oAuthProvider = OAuthFactory.BuildOAuthProvider(clientProvider, tokenProvider, scopes, passwordProvider: passwordProvider);
+    }
+
+    [WebInvoke(UriTemplate = "token", Method = "POST"), OperationContract]
+    public OAuthResponse Token()
+    {
+      var context = new HttpOAuthContext<int>(HttpContext.Current);
+      var accessToken = this.oAuthProvider.GrantAccessToken(context);
+      WebOperationContext.Current.OutgoingResponse.StatusCode = accessToken.StatusCode();
+      WebOperationContext.Current.OutgoingResponse.Format = WebMessageFormat.Json;
+      WebOperationContext.Current.OutgoingResponse.Headers["Cache-Control"] = "no-store";
+      var response = new OAuthResponse();
+      if (accessToken.ErrorCode == ErrorCode.None)
+      {
+        response.AccessToken = accessToken.AccessToken.Token;
+        response.RefreshToken = accessToken.AccessToken.RefreshToken;
+        if (accessToken.AccessToken.Expires.HasValue)
+        {
+          response.ExpiresIn =
+            (int)(accessToken.AccessToken.Expires.Value - accessToken.AccessToken.TimeStamp).TotalSeconds;
+        }
+
+      }
+      else
+      {
+        response.Error = accessToken.Error();
+      }
+
+      return response;
+    }
+  }
+}
---
+++ src/DOAP.WCFExample/SampleService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Web;
+using DOAP.WCF;
+
+namespace DOAP.WCFExample
+{
+  // Configures the OAuth handler for this service. the default scope is blank but we can easily change it to something else
+  [OAuthAuthenticationConfiguration(typeof(OAuth.OAuthHandler),true,"")]
+  [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+  [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+  [ServiceContract]
+  public class SampleService
+  {
+    // We could create a base service class that has this property as well as some of the service attributes above.
+    private bool IsAnonymous
+    {
+      get
+      {
+        if (ServiceSecurityContext.Current == null)
+        {
+          return true;
+        }
+
+        return ServiceSecurityContext.Current.PrimaryIdentity == null;
+      }
+    }
+
+    [WebGet(UriTemplate = "Anon"), OperationContract]
+    public string AnonymousCall()
+    {
+      return "This is an anonymous call";
+    }
+
+    // We could use the Operation OAuth attribute here OR the ServiceOAuthAuthentication attribute on the class
+    [WebGet(UriTemplate = "Secure"), OperationContract, OperationOAuthAuthentication]
+    public string SecureCall()
+    {
+      return "This is a secure call";
+    }
+
+    [WebGet(UriTemplate = "SemiSecure"), OperationContract, OperationOAuthAuthentication(allowAnonymous:true)]
+    public string SemiSecureCall()
+    {
+      if(this.IsAnonymous)
+      {
+        return "This is a semi secure call that is anonymous";
+      }
+
+      return "This is a semi secure call that is secure";
+    }
+  }
+}
